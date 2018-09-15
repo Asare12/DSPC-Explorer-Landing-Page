aadf9a341b59290e7a4b5fc2f1f78d4289891ef1
DESKTOP-FIE7QI7\Asare <gohan.david11@gmail.com>
2018-09-15 21:35:57 +0100

    Committing project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    function myFunction(){
+        var x = document.getElementById("advance_search_form");
+        if(x.style.display === "block") {
+            x.style.display = "none";
+        }else {
+            x.style.display = "block";
+        }
+    }
